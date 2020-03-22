995ce7b01ada2f0806bce152f9a8e5c7695b399e
knightvertrag <52249024+knightvertrag@users.noreply.github.com>
2020-03-22 17:20:08 +0530

    design built

--- lib/game_stage.dart
+++ lib/game_stage.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hangman/character_picker.dart';
+import 'package:hangman/hangman_painter.dart';
+import 'package:hangman/puzzle.dart';
 
-// A custom painter for the app. 
-class HangmanPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint();
-    paint.color = Colors.grey;
-    paint.style = PaintingStyle.fill;
 
-    _drawFrame(canvas, size, paint);
-    _drawNoose(canvas, size, paint);
-  }
 
-  _drawNoose(Canvas canvas, Size size, Paint paint) {
-    var nooseStart = Offset(size.width / 2, 0);
-    var nooseEnd = Offset(size.width / 2, size.height / 5);
-    paint.strokeWidth = 8.0;
-    canvas.drawLine(nooseStart, nooseEnd, paint);
-  }
-
-  _drawFrame(Canvas canvas, Size size, Paint paint) {
-    canvas.drawRect(Rect.fromLTRB(0, size.height, 12, 0), paint);
-
-    canvas.drawRect(Rect.fromLTRB(0, 0, size.width / 2, 12), paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
 
 // Root widget.
 class GameStage extends StatefulWidget {
@@ -70,7 +45,14 @@ class _GameStageState extends State<GameStage> {
               ),
               Expanded(
                 child: Container(
-                  child: Center(child: Text("Hangman")),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      CharacterPicker(),
+                      Puzzle(guessWord: "HANGMAN",)
+                    ],
+                  ),
                 ),
               )
             ],
